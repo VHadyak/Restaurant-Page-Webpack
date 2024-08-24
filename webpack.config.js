873a3ae052498b7426b84a3a55adb6f6816f82e2
vladh <hadyak305246@gmail.com>
2024-08-24 17:50:36 -0600

    Configure html with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -8,4 +9,9 @@ module.exports = {
     path: path.resolve(__dirname, "dist"), // create a dist folder with bundle.js
     clean: true, // only show the most recent bundling code
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html", // generate html file in dist directory with injected js bundle
+    }),
+  ],
 }
