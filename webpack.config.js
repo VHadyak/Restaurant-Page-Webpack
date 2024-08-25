fa3ae3c444c9585dd0eef1ffdd5f3c8a569202bf
vladh <hadyak305246@gmail.com>
2024-08-24 21:24:12 -0600

    Add a Montserrat font and configure it with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
+  entry: "./src/index.js", 
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"), // create a dist folder with bundle.js
@@ -29,6 +29,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"], // Translate CSS into JS module, then take JS module and inject CSS into html
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
       },
     ],
   },
