bc91fd43c6302832ea2db312be3c1c5e8c83aa05
vladh <hadyak305246@gmail.com>
2024-08-29 16:28:08 -0600

    Implement tabbed browsing to navigate between pages

--- src/index.js
+++ src/index.js
@@ -6,6 +6,42 @@ import renderHome from "./home.js";
 import renderMenu from "./menu.js";
 import renderContact from "./contact.js";
 
-//renderHome();
-//renderMenu();
-renderContact();
+function clearContent() {
+  const content = document.querySelector("div#content");
+  content.innerHTML = "";
+  return content;
+};
+
+renderHome();
+
+const buttons = document.querySelectorAll("button");
+
+// Tabbed browsing with Nav
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+
+    // Remove border highlight from all the buttons
+    buttons.forEach(btn => {
+      const span = btn.querySelector("span");
+      console.log(span);
+      if (span) span.classList.remove("highlight");
+    });
+    
+    // Keep border highlight on the btn that was currently clicked
+    const clickedSpan = button.querySelector("span");
+    if (clickedSpan) clickedSpan.classList.add("highlight");
+
+    // Clear content and load the currently clicked one
+    if (button.textContent === "Contact") {
+      clearContent();
+      renderContact();
+    } else if (button.textContent === "Menu") {
+      clearContent();
+      renderMenu();  
+    } else {
+      clearContent();
+      renderHome();
+    };
+  })
+});
+
